62045d1c218532c61ae44ab1750c23667cd4d75a
hugao-ao <hugoviana91@gmail.com>
2025-04-24 15:49:39 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,5 +3,5 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2"
 
 export const supabase = createClient(
   "https://siteteste.supabase.co",
-  "SUA_PUBLIC_API_KEY_AQUI"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiaWtza2Jma2h1bmRocm9weWtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MTk5NjEsImV4cCI6MjA2MTA5NTk2MX0.-n-Tj_5JnF1NL2ZImWlMeTcobWDl_VD6Vqp0lxRQFFU"
 );
