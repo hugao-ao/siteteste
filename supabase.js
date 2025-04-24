60c5b72e73dca29bfd8844621b0c3521181ecc1e
hugao-ao <hugoviana91@gmail.com>
2025-04-24 16:24:28 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 // Arquivo de integração com Supabase
-import { createClient } from "https://vbikskbfkhundhropykf.supabase.co"
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2"
 
 export const supabase = createClient(
   "https://vbikskbfkhundhropykf.supabase.co",
