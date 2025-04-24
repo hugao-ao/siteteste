474007c32159d2933cedf5ca6284ea7bc77a119d
hugao-ao <hugoviana91@gmail.com>
2025-04-24 16:41:56 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -5,3 +5,4 @@ export const supabase = createClient(
   "https://vbikskbfkhundhropykf.supabase.co",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiaWtza2Jma2h1bmRocm9weWtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MTk5NjEsImV4cCI6MjA2MTA5NTk2MX0.-n-Tj_5JnF1NL2ZImWlMeTcobWDl_VD6Vqp0lxRQFFU"
 );
+window.supabase = supabase;
