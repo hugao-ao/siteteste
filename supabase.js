031be239990872428bf4e4196240250ef57efab9
hugao-ao <hugoviana91@gmail.com>
2025-04-29 21:13:35 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,8 @@
-// Arquivo de integração com Supabase
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2"
+// supabase.js
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-export const supabase = createClient(
-  "https://vbikskbfkhundhropykf.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiaWtza2Jma2h1bmRocm9weWtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MTk5NjEsImV4cCI6MjA2MTA5NTk2MX0.-n-Tj_5JnF1NL2ZImWlMeTcobWDl_VD6Vqp0lxRQFFU"
-);
-window.supabase = supabase;
+// Substitua pela URL e pela anon key do seu projeto Supabase
+const SUPABASE_URL = "https://vbikskbfkhundhropykf.supabase.co";
+const SUPABASE_ANON_KEY =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiaWtza2Jma2h1bmRocm9weWtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MTk5NjEsImV4cCI6MjA2MTA5NTk2MX0.-n-Tj_5JnF1NL2ZImWlMeTcobWDl_VD6Vqp0lxRQFFU";
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
