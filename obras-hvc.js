2d9e35d0a60aeede22f488b75228b5c5eb10ed44
hugao-ao <hugoviana91@gmail.com>
2025-07-29 09:27:05 -0300

    Update obras-hvc.js

--- obras-hvc.js
+++ obras-hvc.js
@@ -1064,7 +1064,7 @@ class ObrasManager {
             const percentualConclusao = obra.percentual_conclusao || 0;
             
             // 🎯 CORREÇÃO: Mostrar valor correto na lista
-            const valorObra = obra.valor_total ? (obra.valor_total / 100) : 0;
+            const valorObra = obra.valor_total ? (obra.valor_total) : 0;
             
             const row = document.createElement('tr');
             row.innerHTML = `
