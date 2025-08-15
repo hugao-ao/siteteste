f7e2b1ef436c4794a1075afa9d5c7c984582910d
hugao-ao <hugoviana91@gmail.com>
2025-08-15 18:28:33 -0300

    Update medicoes-hvc.js

--- medicoes-hvc.js
+++ medicoes-hvc.js
@@ -249,10 +249,18 @@ class MedicoesManager {
                                 const propostaId = propostasContratadas[0].id;
                         
                                 // Buscar serviços da proposta
-                                const { data: servicos, error: servicosError } = await supabaseClient
-                                    .from('itens_proposta_hvc')
-                                    .select('*')
-                                    .eq('proposta_id', propostaId);
+                                        const { data: servicos, error: servicosError } = await supabaseClient
+                                            .from('servicos_andamento')
+                                            .select(`
+                                                *,
+                                                itens_proposta_hvc (
+                                                    codigo,
+                                                    descricao,
+                                                    detalhe
+                                                )
+                                            `)
+                                            .eq('obra_id', obraId);
+                                        
                         
                                 if (servicosError) throw servicosError;
                         
